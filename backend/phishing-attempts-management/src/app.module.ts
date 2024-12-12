753d6c9c077b4640b01a7a29dee00c34f8b62d07
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2024-12-12 15:22:36 +0400

    implement Auth module

--- backend/phishing-attempts-management/src/app.module.ts
+++ backend/phishing-attempts-management/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './common/env.validation';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
